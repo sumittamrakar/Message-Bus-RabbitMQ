00bb3283d274a0cd10ed8f284d750621e733ae5e
Sumit Tamrakar <sumit.tamrakar@terumobct.com>
2019-08-30 14:27:42 -0600

    Added a sample helloworld project to use publish/subscribe RabbitMQ

---
+++ rabbitmq1/rabbitmq_receive/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace rabbitmq_receive
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         ConnectionFactory factory = new ConnectionFactory();
+         factory.HostName = "127.0.0.1";
+         // Use predefined username and password or one obtained when rabbitMQ was configured
+         factory.UserName = "user";
+         factory.Password = "0hGxXRatU3"; 
+         //factory.VirtualHost = "/";
+
+         using (IConnection connection = factory.CreateConnection())
+         {
+            using (IModel channel = connection.CreateModel())
+            {
+               channel.QueueDeclare(queue: "myq", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+               var consumer = new EventingBasicConsumer(channel);
+               consumer.Received += Consumer_Received;
+               channel.BasicConsume(queue: "myq", autoAck: true, consumer: consumer);
+               Console.ReadLine();
+            }
+         }
+
+      }
+
+      private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+      {
+         var body = e.Body;
+         string msg = System.Text.Encoding.UTF8.GetString(body);
+         Console.WriteLine($"received: {msg}");
+      }
+   }
+}
---
+++ rabbitmq1/rabbitmq_send/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace rabbitmq_receive
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         ConnectionFactory factory = new ConnectionFactory();
+         factory.HostName = "127.0.0.1";
+         // Use predefined username and password or one obtained when rabbitMQ was configured
+         factory.UserName = "user";
+         factory.Password = "0hGxXRatU3";
+         //factory.VirtualHost = "/";
+
+         using (IConnection connection = factory.CreateConnection())
+         {
+            using (IModel channel = connection.CreateModel())
+            {
+               channel.QueueDeclare(queue: "myq", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+               string msg = "Hello, World! It's Sumit.";
+               var body = System.Text.Encoding.UTF8.GetBytes(msg);
+
+               channel.BasicPublish(exchange: "", routingKey: "myq", basicProperties: null, body: body);
+
+               Console.WriteLine($"sent: {msg}");
+            }
+         }
+
+      }
+   }
+}
